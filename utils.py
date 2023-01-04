67b1a71a83b3fb23c3e553579fcc02e5931940dc
sushanthsk2004 <sushanthsk2004@berkeley.edu>
2023-01-03 21:30:20 -0800

    finished commenting utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,26 @@
+"""This module is used to read and write to the "cache/high_scores.txt"
+file, which records the user's highest score in each of the three difficulty
+modes "Easy", "Medium" and "Hard". 
+"""
+
 WORST_TIME = 1000
 DIFFICULTIES = ["Easy", "Medium", "Hard"]
 
 def read_high_scores(file):
+    """Fetches rows from a file. Retrieves the rows of the file and formats 
+    it into a dictionary. Assumes that the file is formatted as a set of 
+    comma seperated values, and that each line has exactly two values
+
+    Args:
+        file -- A string; the filepath to the file to be read
+
+    Returns:
+        A dict mapping the first entry of each line to the second entry of each line 
+
+    Raises:
+        FileNotFoundError: No such file or directory
+        IndexError: list index out of range
+    """
     f = open(file)
     scores = {}
     lines = f.readlines()
@@ -12,11 +31,32 @@ def read_high_scores(file):
     return scores 
         
 def write_high_scores(scores, file):
+    """Writes the scores to the file, following the format described for the 
+    input in the read_high_scores method. 
+
+    Args:
+        scores -- A dictionary; a map between the difficulty and the user's best
+        score in each category. 
+        file -- A string; the filepath to the file to be written to
+
+    Raises:
+        FileNotFoundError: No such file or directory
+        IndexError: list index out of range
+    """
     f = open(file, "w")
     for key, value in scores.items():
         f.write(key + ", " + str(value) + "\n")
     
 def reset_high_scores(file):
+    """Sets the best score in each category equal to WORST_TIME, and writes
+    to FILE
+
+    Args:
+        file -- A string; the filepath to the file to be written to
+
+    Raises:
+        FileNotFoundError: No such file or directory
+    """
     f = open(file, "w")
     for difficulty in DIFFICULTIES:
         f.write(difficulty + ", " + str(WORST_TIME) + "\n")
