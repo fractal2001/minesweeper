ff9c4cdce34bf1e1a21185f4ef0e7daac6daa176
sushanthsk2004 <sushanthsk2004@berkeley.edu>
2023-01-05 18:13:49 -0800

    done commenting

--- canvas.py
+++ canvas.py
@@ -1,45 +1,99 @@
 import random
+from collections import deque
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import QColor
 from PyQt6.QtCore import *
-from collections import deque
 from tiles import *
+
+"""Global Variables:
+
+UNEXPOSED_DARK: Dark color to use before tile has been revealed
+UNEXPOSED_LIGHT: Light color to use before tile has been revealed
+UNEXPOSED_HIGHLIGHT: The hover color for the tiles.
+EXPOSED_DARK: Dark color to use when a tile has been revealed
+EXPOSED_LIGHT: Light color to use when a tile has been revealed
+"""
 
 UNEXPOSED_DARK = QColor("#D78521")
 UNEXPOSED_LIGHT = QColor("#F2D398")
 UNEXPOSED_HIGHLIGHT = QColor("#E6B062")
-
-EXPOSED_LIGHT = QColor("#E8EBF7")
 EXPOSED_DARK = QColor("#D5DBF0")
+EXPOSED_LIGHT = QColor("#E8EBF7")
 
 class Canvas(QGraphicsScene):
+    """A Canvas object that represents the board of minesweeper. 
+    
+    width -- the number of tiles wide the board should be
+    height -- the number of tiles high the board should be
+    num_bombs -- the number of bombs on the board
+    tile_size -- the size of each tile on the board
+    grid -- a 2D array storing Tile references for each position on the grid
+    game_over -- a boolean that tracks if the game is over
+    game_won -- a boolean tha tracks if the game is won
+    safes -- a deque consisting of all incorrectly flagged tiles. Note that
+    this attribute is only ever computed when the player loses the game
+    bombs -- a deque consisting of all bomb tiles that were unmarked. Note that
+    this attribute is only ever computer when the player loses the game. 
+    """
     def __init__(self, width, height, tile_size, num_bombs):
+        """Create a Canvas object that is WIDTH tiles wide, HEIGHT tiles high, where each tile has 
+        size TILE_SIZE, and the grid has NUM_BOMB randomly placed bombs.
+        
+        width -- An integer; number of tiles wide
+        height -- An integer; number of tiles high
+        tile_size -- An integer; the size of a tile
+        num_bombs -- An integer; the number of bombs that should be randomly placed in the grid
+        """
+        # Reset the class attributes of Tile each time a new Canvas object is initialized.
+        # Each time a new Canvas a object is initialized, a new game has been started so these
+        # variables must be reset for the current game.
+        Tile.num_flagged_cells = 0
+        Tile.first_move_made = False
+
         self.width = width 
         self.height = height 
         self.num_bombs = num_bombs
         self.tile_size = tile_size
-        super().__init__(0, 0, width * tile_size, height * tile_size)
+        # We will update self.grid to contain the Tile references when we call the self._randomize() method
         self.grid = [ [None] * width for i in range(height) ]
         self.game_over = False
         self.game_won = False
-
-        self.randomize()
-        self.compute_counts()
-
+        # Initialize the scene to be width * tile_size pixels wide and height * tile_size pixels tall
+        super().__init__(0, 0, width * tile_size, height * tile_size)
+        # Randomize the tiles, and set the count values for each of the safe tiles
+        self._randomize()
+        self._compute_counts()
+        # To be used for self._end_game_sequence()
         self.safes = deque()
         self.bombs = deque()
-    # y comes from top, x comes from left, but x goes first
-    def randomize_around_start(self, x, y):
-        pass
 
     def game_finished(self):
+        """Returns a boolean on whether or not the game is over
+        
+        Returns:
+            A boolean representing whether or not the game is over
+        """
         return self.game_over
     
     def game_is_won(self):
+        """Returns a boolean on whether or not the game has been won
+        
+        Returns:
+            A boolean representing whether or not the game is won
+        """
         return self.game_won
 
     def _compute_count_at(self, x, y):
-        assert self.grid[y][x].is_safe(), "Current tile is not safe" 
+        """Sets the number of bombs surrounding the tile located at position (X,Y)
+        
+        Args:
+            x -- the x-coordinate of the tile measured from the left
+            y -- the y-cooridnate of the tile measured from the top
+
+        Raises:
+            IndexError, AssertionError
+        """
+        assert self.grid[y][x].is_safe(), "Current tile is not safe." 
 
         dy = [0, 0, 1, 1, 1, -1, -1, -1]
         dx = [-1, 1, -1, 0, 1, -1, 0, 1]
@@ -52,19 +106,31 @@ class Canvas(QGraphicsScene):
                 count += 1
         self.grid[y][x].set_num_bombs(count)
 
-    def compute_counts(self):
+    def _compute_counts(self):
+        """Sets the number of bombs surrounding each SafeTile on the board."""
         for i in range(self.width):
             for j in range(self.height):
                 if self.grid[j][i].is_safe():
                     self._compute_count_at(i, j)
+    
+    def _randomize_around_start(self, x, y):
+        # TODO: The implementation of this function should essentially guarantee that on the first 
+        # move of the game, the user does not pick a bomb. To ensure that the game is actually solvable,
+        # it should also ensure that the generated floodfill region is concave, so that some tiles
+        # can be immediately flagged as bombs. The arguments X, and Y should represent the location of 
+        # the user's first move (here the first move must be a move where the user exposes a tile).  
+        pass
 
-    def randomize(self):
+    def _randomize(self):
+        """Randomly assigns each grid position to a BombTile or a SafeTile while ensuring that
+        there are exactly self.num_bombs number of BombTiles."""
+        # 1 represents a BombTile, and 0 represents a SafeTile
         indicator = [1] * self.num_bombs + [0] * (self.width * self.height - self.num_bombs)
         random.shuffle(indicator)
         index = 0
-
         for i in range(self.width):
             for j in range(self.height):
+                # The modulo 2 is here to make a chess-board like pattern
                 if (i + j) % 2 == 0:
                     if indicator[index] == 0:
                         tile = SafeTile(UNEXPOSED_DARK, UNEXPOSED_HIGHLIGHT, EXPOSED_DARK, self.tile_size)
@@ -75,13 +141,21 @@ class Canvas(QGraphicsScene):
                         tile = SafeTile(UNEXPOSED_LIGHT, UNEXPOSED_HIGHLIGHT, EXPOSED_LIGHT, self.tile_size)
                     else:
                         tile = BombTile(UNEXPOSED_LIGHT, UNEXPOSED_HIGHLIGHT, self.tile_size)
+                # Add tiles to the QGraphicsScene
                 tile.setPos(i * self.tile_size, j * self.tile_size)
                 self.addItem(tile)
                 self.grid[j][i] = tile
                 index += 1
     
-    def floodfill(self, x, y):
-        assert self.grid[y][x].is_safe() and self.grid[y][x].get_num_bombs() == 0
+    def _floodfill(self, x, y):
+        """Exposes every possible safe tile when a tile that is surrounded by zero bombs is selected.
+        
+        Args:
+            x -- the x-coordinate of the tile measured from the left
+            y -- the y-cooridnate of the tile measured from the top
+        """
+        assert self.grid[y][x].is_safe() and self.grid[y][x].get_num_bombs() == 0, "Tile cannot be floodfilled."
+        # Just a standard DFS implementation 
         fringe = deque()
         fringe.append((x, y))
         visited = set()
@@ -92,6 +166,7 @@ class Canvas(QGraphicsScene):
             self.grid[ytop][xtop].force_expose()
             visited.add((xtop, ytop))
             if self.grid[ytop][xtop].get_num_bombs() == 0:
+                # Add any neighbors to the stack
                 dy = [0, 0, 1, 1, 1, -1, -1, -1]
                 dx = [-1, 1, -1, 0, 1, -1, 0, 1]
                 for i in range(len(dy)):
@@ -102,6 +177,8 @@ class Canvas(QGraphicsScene):
                         fringe.append((x_new, y_new))
         
     def _get_unmarked_bombs(self):
+        """Populates self.bombs with all of the unmarked bombs (i.e. the bombs unknown to the user).
+        To be used when the game is lost, and all the bombs need to be exposed."""
         for i in range(self.width):
             for j in range(self.height):
                 if not self.grid[j][i].is_safe() and not self.grid[j][i].flagged():
@@ -109,6 +186,9 @@ class Canvas(QGraphicsScene):
         random.shuffle(self.bombs)
     
     def _get_incorrectly_marked_safe(self):
+        """Populates self.safes with all of the incorrectly flagged tiles (i.e. the safe tiles that 
+        were flagged). To be used when the game is lost, and all the incorrectly flagged tiles 
+        need to be exposed."""
         for i in range(self.width):
             for j in range(self.height):
                 if self.grid[j][i].is_safe() and self.grid[j][i].flagged():
@@ -116,6 +196,9 @@ class Canvas(QGraphicsScene):
         random.shuffle(self.safes)
     
     def _end_game_sequence(self):
+        """Force exposes all tiles in self.bombs and self.safes, with bombs being exposed every
+        200, 300, or 400 milliseconds and safe tiles being crossed out every 50, 100, or 150 milliseconds.
+        The QTimer is used to animate this process."""
         if len(self.bombs) != 0:
             self.bombs.pop().force_expose()
             QTimer.singleShot(random.choice([200, 300, 400]), self._end_game_sequence)
@@ -124,15 +207,19 @@ class Canvas(QGraphicsScene):
             QTimer.singleShot(random.choice([50, 100, 150]), self._end_game_sequence)
                         
     def _disable_mouse_events(self):
+        """Disable all tiles from accepting mouse events."""
         for i in range(self.width):
             for j in range(self.height):
                 self.grid[j][i].setAcceptHoverEvents(False)
                 self.grid[j][i].setActive(False)
                 self.grid[j][i].setAcceptedMouseButtons(Qt.MouseButton.NoButton)
     
-    def check_win_condition(self):
+    def _check_win_condition(self):
+        """Checks if the game has been won and updates the self.game_over and self.game_won
+        attributes accordingly. Disables mouse events if the game has been won."""
         for i in range(self.width):
             for j in range(self.height):
+                # If there is an unexposed safe tile, the game is not over
                 if self.grid[j][i].is_safe() and not self.grid[j][i].is_exposed():
                     return 
         self.game_over = True
@@ -140,33 +227,43 @@ class Canvas(QGraphicsScene):
         self._disable_mouse_events()
 
     def mousePressEvent(self, event):
+        """Handler for mouse press events. Mouse events are also handled here for floodfilling
+        and ending the game after it has been won or lost. 
+
+        Args:
+            event -- A QGraphicsSceneMouseEvent to handle
+        """
         if self.game_over:
             return 
         if event.button() == Qt.MouseButton.LeftButton:
             point = event.buttonDownScenePos(Qt.MouseButton.LeftButton)
             x = int(point.x() // self.tile_size)
             y = int(point.y() // self.tile_size)
+            # First, process the event at the Tile level
             self.grid[y][x].mousePressEvent(event)
-
+            # If the user presses a bomb that is not flagged, the game is over
             if not self.grid[y][x].is_safe() and not self.grid[y][x].flagged():
+                # Disable mouse events before starting to explode the bombs and cross out
+                # incorrectly flagged tiles, since there have been bugs where the user can
+                # still click on tiles during self._end_game_sequence()
                 self._disable_mouse_events()
-
+                # Populate self.bombs and self.safes
                 self._get_unmarked_bombs()
                 self._get_incorrectly_marked_safe()
-
+                # Animate the explosions and crossouts
                 self._end_game_sequence()
-
                 self.game_over = True
                 return 
+            # If the user clicks on a safe tile with no bombs, floodfill it 
             elif self.grid[y][x].is_safe() and self.grid[y][x].get_num_bombs() == 0 \
                  and not self.grid[y][x].flagged():
-                self.floodfill(x, y)
+                self._floodfill(x, y)
+        # If you right click, then the Tile.mousePressEvent handles it perfectly.
         elif event.button() == Qt.MouseButton.RightButton:
             point = event.buttonDownScenePos(Qt.MouseButton.RightButton)
             x = int(point.x() // self.tile_size)
             y = int(point.y() // self.tile_size)
             self.grid[y][x].mousePressEvent(event)
-        self.check_win_condition()
+        # Check if the game has been won after each mouse click
+        self._check_win_condition()
     
-    def update_parameters(self, width, height, tile_size, num_bombs):
-        self.__init__(width, height, tile_size, num_bombs)
--- main.py
+++ main.py
@@ -6,10 +6,23 @@ from dialog import *
 from stopwatch import *
 from PyQt6.QtGui import QFont
 
+"""Global Variables:
+
+MODES: Contains three different difficulties "Easy", "Medium" and "Hard". Each tuple is of the form
+(number of tiles wide, number of tiles high, tile size, number of bombs)
+ICON_SIZE: Controls the size of all images displayed in the UI
+TEXT_SIZE: Controls the size of the font of text in the UI
+SCORES_FILE_PATH: The filepath to the high_scores.txt file
+FLAG_FILE_PATH: The filepath to the flag icon (to be used for displaying the number of flagged tiles)
+TROPHY_FILE_PATH: The filepath to the trophy icon (to be used to display user's best score)
+HIGH_SCORES: The current user's high scores, that were stored in SCORES_FILE_PATH
+MAX_TIME: The timeout for the timer. This is the worst possible time a player can take. 
+"""
+
 MODES = {
-    'Hard' : (24, 20, 32, 99),
-    'Medium' : (18, 14, 46, 40),
-    'Easy' : (10, 8, 50, 10), 
+    "Easy": (10, 8, 50, 10),
+    "Medium" : (18, 14, 46, 40),
+    "Hard" : (24, 20, 32, 99),
 }
 ICON_SIZE = 30
 TEXT_SIZE = 15
@@ -18,45 +31,68 @@ FLAG_FILE_PATH = "images/flag.png"
 TIME_FILE_PATH = "images/hourglass.png"
 TROPHY_FILE_PATH = "images/trophy.png"
 HIGH_SCORES = read_high_scores(SCORES_FILE_PATH)
+MAX_TIME = 999
 
 class MainWindow(QMainWindow):
+    """A MainWindow object that contains the minesweeper game along with
+    a timer, difficulty-setter, and a win/lose dialog box. 
+    
+    mode -- the difficulty; "Easy", "Medium" or "Hard".
+    dialog_displayed -- a boolean that checks if the dialog box is displayed or not. 
+    This was used to fix a bug, where when the dialog box was displayed, the user could click
+    on the minesweeper game to make the dialog box redraw itself. 
+    total_bombs -- the total number of bombs in the game
+    scene -- A canvas object containing the minesweeper game
+    view -- A QGraphicsView object, which renders self.scene
+    scores -- the user's highest scores stored as a dictionary, with times for each category
+    watch -- A stopwatch object to track how much time the user has elapsed since the start
+    timer_active -- A boolean which checks if self.watch is currently running
+    flag_count -- A QLabel object, which displays the number of currently flagged cells
+    difficulty_box -- A QComboBox object, which contains the different difficulty modes for 
+    the user to select from
+    """
     def __init__(self, mode):
-        """Create a MainWindow object for user interaction"""
-        super(MainWindow, self).__init__()
+        """Create a MainWindow object with difficulty MODE
+        
+        mode -- the desired difficulty; "Easy", "Medium" or "Hard". 
+        """
+        super().__init__()
+        self.setWindowTitle("Minesweeper")
         self.mode = mode
         self.dialog_displayed = False
-        self.width, self.height, self.tile_size, self.total_bombs = MODES[mode]
-        self.setWindowTitle("Minesweeper")
-        self.scene = Canvas(self.width, self.height, self.tile_size, self.total_bombs)
+        width, height, tile_size, self.total_bombs = MODES[mode]
+        self.scene = Canvas(width, height, tile_size, self.total_bombs)
         self.view = QGraphicsView(self.scene)
         self.scores = read_high_scores(SCORES_FILE_PATH)
 
+        # Vectorizes graphics
+        self.view.setRenderHint(QPainter.RenderHint.Antialiasing)        
         self.view.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
         self.view.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
 
-        # Vectorizes graphics
-        self.view.setRenderHint(QPainter.RenderHint.Antialiasing)
-
-        self.watch = StopWatch(999, TIME_FILE_PATH, ICON_SIZE, TEXT_SIZE)
+        self.watch = StopWatch(MAX_TIME, TIME_FILE_PATH, ICON_SIZE, TEXT_SIZE)
+        # Don't start the timer immediately
         self.timer_active = False 
 
         flag_icon = QLabel()
         flag_icon.setPixmap(QPixmap(FLAG_FILE_PATH).scaled(ICON_SIZE, ICON_SIZE))
 
-        self.flag_count = QLabel()
-        self.flag_count.setText("0/" + str(self.total_bombs))
+        # An updating display of the number of flags put down by the user
         font = QFont()
         font.setPointSize(TEXT_SIZE)
+        self.flag_count = QLabel()
+        self.flag_count.setText("0/" + str(self.total_bombs))
         self.flag_count.setFont(font)
 
+        # The flag_widget consists of the flag icon next to the flag count
         flag_layout = QHBoxLayout()
         flag_layout.addWidget(flag_icon)
         flag_layout.addWidget(self.flag_count)
         flag_layout.setSpacing(8)
-
         flag_widget = QWidget()
         flag_widget.setLayout(flag_layout)
 
+        # Customizes the difficulty setter box
         self.difficulty_box = QComboBox()
         font.setPointSize(TEXT_SIZE - 2)
         self.difficulty_box.setFont(font)
@@ -65,73 +101,109 @@ class MainWindow(QMainWindow):
         self.difficulty_box.setCurrentIndex(list(MODES.keys()).index(mode))
         self.difficulty_box.currentTextChanged.connect(self.difficulty_setter)
 
+        # Sets the layout of all the widgets 
         button_layout = QGridLayout()
         button_layout.setSpacing(0)
         button_layout.addWidget(self.difficulty_box, 0, 4, Qt.AlignmentFlag.AlignRight)
         button_layout.addWidget(self.watch, 0, 5, Qt.AlignmentFlag.AlignCenter)
         button_layout.addWidget(flag_widget, 0, 6, Qt.AlignmentFlag.AlignLeft)
-
         full_layout = QVBoxLayout()
         full_layout.addLayout(button_layout)
         full_layout.addWidget(self.view)
 
+        # Pack the full layout into a widget to display
         interface = QWidget()
         interface.setLayout(full_layout)
         self.setCentralWidget(interface)
     
     def flag_count_update(self):
+        """Updates the text for the number of flagged cells"""
         self.flag_count.setText(str(Tile.num_flagged_cells) + "/" + str(self.total_bombs))
         self.flag_count.update()
     
     def _customize_win_dialog(self):
+        """Returns a WinDialog object with the user's time for the current game as well as their 
+        best time for that game mode. 
+        
+        Returns:
+            A WinDialog object containing their current and best times
+        """
         cur_time = self.watch.get_time()
         best_time = HIGH_SCORES[self.mode]
+        # If their cur_time is greater than their best_time, their best_time is still best_time
         if cur_time >= best_time:
             return WinDialog(cur_time, best_time, TIME_FILE_PATH, TROPHY_FILE_PATH)
         return WinDialog(cur_time, cur_time, TIME_FILE_PATH, TROPHY_FILE_PATH)
     
     def _save_best_time(self):
+        """Updates the HIGH_SCORES dictionary and saves the dictionary to SCORES_FILE_PATH"""
         cur_time = self.watch.get_time()
         HIGH_SCORES[self.mode] = min(cur_time, HIGH_SCORES[self.mode])
         write_high_scores(HIGH_SCORES, SCORES_FILE_PATH)
         
     def mousePressEvent(self, event):
+        """Handler for mouse press events. Mouse events are handled here in order to reset the timer
+        and update the number of flagged tiles. 
+
+        Args:
+            event -- A QGraphicsSceneMouseEvent to handle
+        """
+        # Every time the user clicks something, update the flag count
         self.flag_count_update()
+        # Start the timer if the first move has been made, and the timer
+        # has not already been started
         if not self.timer_active and Tile.first_move_made:
             self.watch.start()
             self.timer_active = True
+        # Display dialog if the game is over and dialog has not been displayed yet
         if self.scene.game_finished() and not self.dialog_displayed:
+            # Stop watch and disable the difficulty chooser once game ends
             self.watch.stop()
             self.difficulty_box.setEnabled(False)
+    
             if self.scene.game_is_won():
+                # If the game is won, save the times
                 self.dialog = self._customize_win_dialog()
                 self._save_best_time()
             else:
                 self.dialog = LoseDialog(HIGH_SCORES[self.mode], TIME_FILE_PATH, TROPHY_FILE_PATH)
+            # If the user wants to try again, reset the game and allow them to try again.
+            # Otherwise, allow them to view the endgame_sequence, and they can choose another
+            # difficulty from the difficulty chooser. 
             self.dialog_displayed = True
             self.dialog.show()
             self.dialog.accepted.connect(self.reset_game)
-        super().mousePressEvent(event)
-    
-    def reset_game(self):
-        self.difficulty_box.setEnabled(True)
-        self.difficulty_setter(self.mode)
+            self.dialog.rejected.connect(self.allow_endgame_sequence)
     
     def difficulty_setter(self, new_mode):
-        # reset number of flagged cells, since new instance of Canvas
-        Tile.num_flagged_cells = 0
-        Tile.first_move_made = False
-
+        """Switches the difficulty of the game by setting it to NEW_MODE and starts
+        a new blank game in that new mode.
+        
+        Args:
+            new_mode -- the new difficulty that the user selects
+        """
         self.timer_active = False 
         self.dialog_displayed = False
         self.watch.reset()
-
-        self.width, self.height, self.tile_size, self.total_bombs = MODES[new_mode]
+        width, height, tile_size, self.total_bombs = MODES[new_mode]
+        # Re-initializing the canvas automatically resets the Tile.first_move_made
+        # and the Tile.num_flagged_cells attributes 
+        self.scene = Canvas(width, height, tile_size, self.total_bombs)
         self.mode = new_mode
+        # Re-enable the difficulty box
+        self.difficulty_box.setEnabled(True)
+        # Resets the flag count display to 0
         self.flag_count_update()
-        self.scene = Canvas(self.width, self.height, self.tile_size, self.total_bombs)
         self.view.setScene(self.scene)
         self.view.fitInView(self.scene.itemsBoundingRect(), Qt.AspectRatioMode.KeepAspectRatio)
+    
+    def reset_game(self):
+        """Resets the current game in the same difficulty setting"""
+        self.difficulty_setter(self.mode)
+    
+    def allow_endgame_sequence(self):
+        """Re-enables the difficulty-chooser after the dialog box is closed out"""
+        self.difficulty_box.setEnabled(True)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
